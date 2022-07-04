a405656ca96753ee809ab2c728d879e328f896b8
Cezary Witkowski <czarecoo@gmail.com>
2022-07-04 22:12:28 +0200

    Started working on second version

---
+++ src/main/java/com/czareg/game/screen/MainMenuScreen.java
@@ -0,0 +1,39 @@
+package com.czareg.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.czareg.game.BucketGame;
+
+public class MainMenuScreen extends ScreenAdapter {
+
+    private final BucketGame game;
+
+    private final OrthographicCamera camera;
+
+    public MainMenuScreen(final BucketGame game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+}
